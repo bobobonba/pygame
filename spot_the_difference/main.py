ea698830f5d45392a8dfb3cf877d66e34b0b19c0
takaha <torikabuto1018@gmail.com>
2022-12-11 17:04:31 +0900

    間違い探しのmainファイルを作成

---
+++ spot_the_difference/main.py
@@ -0,0 +1,26 @@
+import logic
+import validator
+import display
+
+board = logic.GameBoard(10)
+valid = validator.GameValid()
+displayer = display.Display()
+
+displayer.show(board)
+print('間違いを見つけよう')
+
+while True:
+    
+    coordinate = valid.validate(input('入力例: A1'))
+
+    if coordinate is False:
+        print('入力しなおしてください')
+        continue
+    
+    result = board.is_collect(coordinate)
+
+    if result is True:
+        print('正解')
+    else:
+        print('不正解')
+        continue
